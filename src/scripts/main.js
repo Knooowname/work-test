3bf78798240e0262c847820b424eb050ac319f8a
Knooowname <91739752+Knooowname@users.noreply.github.com>
2025-12-01 11:30:40 +0500

    01.12.2025_11:30
    01.12.2025_11:30

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,20 @@
 const tabBtns = document.querySelectorAll('.item_btn')
 const searchInput = document.querySelector('.filters_input')
 const cards = document.querySelectorAll('.main_card_item')
+const filtersBtnMobile = document.querySelector('.main_filters_button')
+const mobileFiltersList = document.querySelector('.filters_list-mobile')
+const mobileTabBtns = document.querySelectorAll('.item_btn-mobile')
 
 searchInput.addEventListener('input', () => {
-    const value = searchInput.value.trim().toLowerCase()
+    const value = searchInput.value.trim().toLowerCase();
 
     cards.forEach((item) => {
-        const title = item.querySelector('.info_title').textContent.toLowerCase()
+        const title = item.querySelector('.info_title').textContent.toLowerCase();
 
         if(title.includes(value)) {
-            item.style.display = ''
+            item.style.display = '';
         } else {
-            item.style.display = 'none'
+            item.style.display = 'none';
         }
     })
 })
@@ -35,3 +38,49 @@ tabBtns.forEach((btn) => {
     });
   });
 });
+
+mobileTabBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const textBtn = btn.textContent.toLowerCase().trim();
+
+  if(textBtn === 'all') {
+    cards.forEach(card => card.style.display = '');
+    return;
+  }
+
+  cards.forEach((card) => {
+    const infoExampleText = card.querySelector('.info_example').textContent.toLowerCase().trim();
+    card.style.display = infoExampleText.includes(textBtn) ? '' : 'none';
+  });
+  });
+});
+
+filtersBtnMobile.addEventListener('click', () => {
+  mobileFiltersList.classList.toggle('active_mobile_filters');
+});
+
+function updateTabCounts() {
+  const counts = {}; 
+
+  cards.forEach(card => {
+    const category = card.querySelector('.info_example')
+      .textContent.trim().toLowerCase();
+    
+    counts[category] = (counts[category] || 0) + 1;
+  });
+
+  const allCount = cards.length;
+
+  tabBtns.forEach(btn => {
+    const name = btn.querySelector('.btn_text').textContent.trim().toLowerCase();
+    const countElement = btn.querySelector('.btn_count');
+
+    if (name === "all") {
+      countElement.textContent = allCount;
+    } else {
+      countElement.textContent = counts[name] || 0;
+    }
+  });
+}
+
+updateTabCounts();
